d3cc3ed71995dacb4d4c5a1d6aea4be766baa844
jorozcop <orojacobo@gmail.com>
2022-12-19 21:44:47 -0500

    AD

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -1,13 +1,8 @@
 <template lang="pug">
 .curso-main-container.pb-3
-  BannerInterno()
+  BannerInterno(icono="fas fa-tasks" titulo="Actividad didáctica")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     
-    .titulo-principal.color-acento-contenido
-      .titulo-principal__numero
-        span
-          i.fas.fa-puzzle-piece
-      h1 Actividad didáctica    
     .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
       .titulo-segundo.color-acento-contenido
         h2 Desarrollo y ejecución de la evaluación de desempeño 
