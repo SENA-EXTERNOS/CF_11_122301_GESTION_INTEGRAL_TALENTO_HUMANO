ce1cf397e7530f8b42b164829206f2e8afdf3543
flizcano <flizcanor@icloud.com>
2023-02-12 19:24:09 -0500

    Se ajusta según guion

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -2,10 +2,10 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-tasks" titulo="Actividad didáctica")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    
+
     .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
       .titulo-segundo.color-acento-contenido
-        h2 Desarrollo y ejecución de la evaluación de desempeño 
+        h2 Desarrollo y ejecución de la evaluación de desempeño
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
@@ -14,14 +14,14 @@
 
         p.mb-4 Apreciado aprendiz, a continuación encontrará una serie de preguntas que deberá resolver, con el objetivo de evaluar la aprehensión de los conocimientos expuestos en este componente formativo.
         .tarjeta.actividad.p-3
-          .row.justify-content-around.align-items-center            
+          .row.justify-content-around.align-items-center
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Selección múltiple
             .col-auto
               a.boton.boton--b(:href="obtenerLink('/actividades/index.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
-                
+
 </template>
 
 <script>
--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -3,14 +3,14 @@
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
-    p El siguiente mapa integra los criterios y especificidades de los conocimientos expuestos en el presente componente formativo.
-    
+    p.mb-5 El siguiente mapa integra los criterios y especificidades de los conocimientos expuestos en el presente componente formativo:
+
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
           img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
       .col-auto
-        a.anexo.mb-4(:href="obtenerLink('/downloads/sintesis.pdf')" target="_blank")
+        a.anexo.mb-4(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
             img(src="@/assets/template/icono-pdf.svg")
           .anexo__texto
